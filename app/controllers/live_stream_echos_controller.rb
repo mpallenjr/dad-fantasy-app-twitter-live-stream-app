e45283c6f8cb8995592837886c0b6a179fec5377
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-17 15:33:54 -0600

    Ready to Deploy.

--- app/controllers/live_stream_echos_controller.rb
+++ app/controllers/live_stream_echos_controller.rb
@@ -1,12 +1,11 @@
 class LiveStreamEchosController < ApplicationController
   def index
     require "json"
+    # OPEN THE FILE THAT HOLDS THE CHUNK
     lines = File.open("out.txt").read
+    # PARSE THE CHUNK 
     data = JSON.parse([lines].to_s)
-    p "data"
-    p data
-
-    # render json: data[0].as_json
+    # RENDER THE CHUNK AS JSON TO BE CALLED 
     render json: data[0]
   end
 end
