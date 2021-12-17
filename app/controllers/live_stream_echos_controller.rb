7232087891495b178ff590fa1f86d503d4af24c8
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-16 21:13:52 -0600

    working server dedicated to stream and echo of tweets to front end.

---
+++ app/controllers/live_stream_echos_controller.rb
@@ -0,0 +1,12 @@
+class LiveStreamEchosController < ApplicationController
+  def index
+    require "json"
+    lines = File.open("out.txt").read
+    data = JSON.parse([lines].to_s)
+    p "data"
+    p data
+
+    # render json: data[0].as_json
+    render json: data[0]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "live_stream_tweets" => "live_stream_tweets#index"
+  get "live_stream_echos" => "live_stream_echos#index"
+
 end
